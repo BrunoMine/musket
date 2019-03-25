c4aa7d450aa186c13978b7be71dd2ed46c5cf11b
BrunoMine <borgesdossantosbruno@gmail.com>
2019-03-25 00:57:54 -0300

    Update crafts and doc

--- README-pt_BR.md
+++ README-pt_BR.md
@@ -5,21 +5,21 @@
 [![forum](https://img.shields.io/badge/minetest-mod-green.svg?style=flat-square)](https://forum.minetest.net)
 [![bower](https://img.shields.io/badge/bower-mod-green.svg?style=flat-square)](https://minetest-bower.herokuapp.com/mods/musket)
 
-## Description
-Add Musket for Minetest
+## Descrição
+Adicionar o mosquete ao Minetest
 
-## Depends
-* Minetest 0.5.0 or higher
+## Requisitos
+* Minetest 0.5.0 ou superior
 * Mod shooter
 
-## License
-See LICENSE.txt for detailed license information LGPL 3.0
+## Licença
+Veja LICENSE.txt para informações detalhadas da licença LGPL 3.0
 
-### Authors of the source code
+### Autores do código fonte
 Originalmente por BrunoMine, Bruno Borges <borgesdossantosbruno@gmail.com> (LGPL 3.0)
 
-### Authors of media (textures, models and sounds)
-All that are not listed here:
+### Autores de mídias (texturas, modelos and sons)
+Todos que não estao listados aqui:
 BrunoMine, Bruno Borges <borgesdossantosbruno@gmail.com> (CC BY-SA 3.0)
 
 musket_shot.ogg - fennelliott - CC0
--- init.lua
+++ init.lua
@@ -6,9 +6,6 @@
 	along with this program.  If not, see <https://www.gnu.org/licenses/>.
   ]]
 
-  
--- Global Table
-musket = {}
 
 local modpath = minetest.get_modpath("musket")
 
@@ -26,6 +23,16 @@ minetest.register_craftitem("musket:musket_ammo", {
 	stack_max = 15,
 })
 
+-- Musket Ammo craft
+minetest.register_craft({
+	output = "musket:musket_ammo 6",
+	recipe = {
+		{"shooter:gunpowder", "", "shooter:gunpowder"},
+		{"", "default:steel_ingot", ""},
+		{"shooter:gunpowder", "default:acacia_wood", "shooter:gunpowder"}
+	}
+})
+
 -- Register Musket shooter
 shooter.register_weapon("musket:musket", {
 	description = S("Musket (Loaded)"),
@@ -38,11 +45,21 @@ shooter.register_weapon("musket:musket", {
 	
 	spec = {
 		rounds = 1.1,
-		range = 20,
+		range = 30,
 		step = 30,
-		tool_caps = {full_punch_interval=1.2, damage_groups={fleshy=6}},
+		tool_caps = {full_punch_interval=1.2, damage_groups={fleshy=4}},
+		groups = {snappy=3, crumbly=3, choppy=3, fleshy=2, oddly_breakable_by_hand=2},
 		sound = "musket_shot",
-		particle = "shooter_bullet.png",
+		bullet_image = "musket_bullet.png",
+		particles = {
+			amount = 8,
+			minsize = 0.25,
+			maxsize = 0.75,
+		},
+	},
+	
+	sounds = {
+		reload = "musket_reload",
 	},
 })
 
@@ -73,4 +90,14 @@ do
 	-- Override item
 	minetest.override_item("musket:musket_loaded", {groups=def.groups, on_use=def.on_use})
 end
+
+-- Musket craft
+minetest.register_craft({
+	output = "musket:musket_loaded",
+	recipe = {
+		{"default:steel_ingot", "", ""},
+		{"", "default:steel_ingot", "musket:musket_ammo"},
+		{"", "group:stick", "default:acacia_wood"}
+	}
+})
 
