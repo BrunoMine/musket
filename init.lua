690015901ef248fda09ea91b3b466eb866934393
stujones11 <stujones111@gmail.com>
2019-04-02 19:09:47 +0100

    Update sounds table

--- init.lua
+++ init.lua
@@ -49,17 +49,16 @@ shooter.register_weapon("musket:musket", {
 		step = 30,
 		tool_caps = {full_punch_interval=1.2, damage_groups={fleshy=4}},
 		groups = {snappy=3, crumbly=3, choppy=3, fleshy=2, oddly_breakable_by_hand=2},
-		sound = "musket_shot",
+		sounds = {
+			reload = "musket_reload",
+			shot = "musket_shot",
+		},
 		bullet_image = "musket_bullet.png",
 		particles = {
 			amount = 8,
 			minsize = 0.25,
 			maxsize = 0.75,
 		},
-	},
-	
-	sounds = {
-		reload = "musket_reload",
 	},
 })
 
